da19e2b24d4cca95ff53ee2546a84e16279c3b67
abhishekrajdhar <rajdhardubey4434@gmail.com>
2025-08-10 01:16:43 +0530

    data preprocessing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'social-app',
+    version= '0.0.0',
+    author= 'Abhishek',
+    author_email= 'rajdhardubey4434@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
